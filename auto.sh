886deff95f127d90d508ced3c84c554c24579dcd
Rohan Kaushik <rohankaushik1@outlook.com>
2023-05-02 16:01:36 +0530

    added bash improve script

---
+++ auto.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+read -rp  "do you want to push or pull: " confirm
+
+if [ "$confirm" == push ]
+then
+    git add .
+    read -rp "Enter commit message: " Message
+    git commit -m "$Message"
+    git push -u origin master
+elif [ "$confirm" == pull ]
+then
+    git pull
+else
+    echo "cannot find specified command"
+fi
+
