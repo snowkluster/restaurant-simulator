16d0714567ecd5b82134cf46708d8605225863f8
Rishav <110629977+Rishavgg@users.noreply.github.com>
2023-05-05 17:02:44 +0530

    Update simulation_parameters.py

--- parameters/simulation_parameters.py
+++ parameters/simulation_parameters.py
@@ -1,11 +1,6 @@
-# multiple of real time
 SIMULATION_SPEED=300
 
-# resources to process order items in parallel
 NUM_COOKS=120
 
-# refresh interval in seconds
 DASHBOARD_REFRESH_INTERVAL=2
 
-
-# need to push changes
